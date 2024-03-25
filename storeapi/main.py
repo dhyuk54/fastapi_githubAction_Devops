cf4e02fdb3c89965b66953760af132641ad02898
dhyuk54 <dhyuk23@gmail.com>
2024-03-25 11:30:21 +0900

    update

--- storeapi/main.py
+++ storeapi/main.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 from storeapi.routers.posts import router as posts_router
 
 app = FastAPI()
-print("start fastapi")
+print("start fastapi123")
 app.include_router(posts_router)
